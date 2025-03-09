2ee8c6b66e3c39e3b8f9c5f2fd6119598f07ad63
RAPTOR7762 <tankaiyiaaron@gmail.com>
2025-03-09 19:05:26 +0800

    Update pushbutton.ino
    Signed-off-by: RAPTOR7762 <tankaiyiaaron@gmail.com>

--- projects /03 Push_Button/pushbutton.ino
+++ projects /03 Push_Button/pushbutton.ino
@@ -1,1 +1,17 @@
+const int led = 13;
+const int button = 2;
 
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT_PULLUP);
+}
+
+void loop() {
+  if (digitalRead(button) == 0) {
+    digitalWrite(led, HIGH);
+  }
+
+  if (digitalRead(button) == 1) {
+    digitalWrite(led, LOW);
+  }
+}
