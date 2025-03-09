b3c6c6ccad3e47f0b4f8be8e0ef2bb88c296d517
RAPTOR7762 <tankaiyiaaron@gmail.com>
2025-03-09 14:34:33 +0800

    Update pulse.ino
    Signed-off-by: RAPTOR7762 <tankaiyiaaron@gmail.com>

--- projects /02 Pulse/pulse.ino
+++ projects /02 Pulse/pulse.ino
@@ -1,1 +1,15 @@
+const int led = 11;
+int fadeAmt = 5
+int br; // Brightness of the led
 
+void setup() {
+  pinMode(led, OUTPUT);
+}
+void loop() {
+  analogWrite(led, br);
+  br = br + fadeAmt;
+
+  if (br <= 0 || br >= 255) {
+    fadeAmt = - fadeAmt;
+  }
+  delay(300);
